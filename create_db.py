1dc660a70345c83a49d8d599f66074e0f1615b38
Lakshyasunildubey <lakshyadubey3716@gmail.com>
2024-07-19 23:19:07 -0400

    File script done.

--- create_db.py
+++ create_db.py
@@ -1,11 +1,13 @@
 """
-Description:
- Creates the people table in the Social Network database
- and populates it with 200 fake people.
-
-Usage:
- python create_db.py
+Name : Lakshya Dubey
+Student ID: 10331244
+Group: Rishi Patel, Bhavya Solanki, Lakshya Dubey 
 """
+
+import sqlite3
+from faker import Faker
+import random
+from datetime import datetime
 import os
 
 # Determine the path of the database
@@ -17,17 +19,49 @@ def main():
     populate_people_table()
 
 def create_people_table():
-    """Creates the people table in the database"""
-    # TODO: Create function body
-    # Hint: See example code in lab instructions entitled "Creating a Table"
-    return
+    
+    conn = sqlite3.connect(db_path)
+    c = conn.cursor()
+    c.execute('''
+        CREATE TABLE IF NOT EXISTS people (
+            id INTEGER PRIMARY KEY,
+            name TEXT NOT NULL,
+            email TEXT NOT NULL,
+            address TEXT NOT NULL,
+            city TEXT NOT NULL,
+            province TEXT NOT NULL,
+            bio TEXT,
+            age INTEGER,
+            created_at DATETIME NOT NULL,
+            updated_at DATETIME NOT NULL
+        )
+    ''')
+    conn.commit()
+    conn.close()
+    print("Created the people's table.")
 
 def populate_people_table():
-    """Populates the people table with 200 fake people"""
-    # TODO: Create function body
-    # Hint: See example code in lab instructions entitled "Inserting Data into a Table"
-    # Hint: See example code in lab instructions entitled "Working with Faker"
-    return
+    
+    conn = sqlite3.connect(db_path)
+    c = conn.cursor()
+    fake = Faker()
+    for _ in range(200):
+        name = fake.name()
+        email = fake.email()
+        address = fake.address().replace("\n", ", ")
+        city = fake.city()
+        province = fake.state()
+        bio = fake.text()
+        age = random.randint(1, 100)
+        created_at = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        updated_at = created_at
+        c.execute('''
+            INSERT INTO people (name, email, address, city, province, bio, age, created_at, updated_at)
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+        ''', (name, email, address, city, province, bio, age, created_at, updated_at))
+    conn.commit()
+    conn.close()
+    print("People table with 200 entries created sucessfully.")
 
 if __name__ == '__main__':
-   main()
+    main()
